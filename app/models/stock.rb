febb2f6f7261745504ebaf4b766991cbf4bac0a7
Student <example@example.com>
2019-08-26 16:42:42 -0700

    This works with values table.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,14 +3,4 @@ class Stock < ApplicationRecord
   has_many :users, through: :portfolios
   has_many :trades
 
-  def daily(symbol)
-    RestClient::Request.execute(
-      method: :get,
-      url: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=#{symbol}&interval=5min&apikey=#{ENV['ALPHA_VANTAGE_API_KEY']}")
-  end
-
-  def get_current_price(symbol)
-    JSON.parse(self.daily(symbol))["Time Series (5min)"].first[1]["4. close"]
-  end
-
 end
--- app/models/value.rb
+++ app/models/value.rb
@@ -1,3 +1,13 @@
+require 'byebug'
 class Value < ApplicationRecord
   belongs_to :stock
+
+  def self.populate_table(symbol)
+    stock = Stock.find_by_symbol(symbol)
+    JSON.parse(stock.daily(symbol))["Time Series (5min)"].each do |t|
+      value = Value.new(:price => t[1]["4. close"], :log_time => Time.now(), :stock_id => stock.id)
+      value.save!()
+    end
+  end
+
 end
