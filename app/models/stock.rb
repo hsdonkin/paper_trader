e735586ca94d730cd21dce693f433797d4d7cce3
Josh Mabry <mabry1985@gmail.com>
2019-08-26 20:50:34 -0700

    edit search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,10 +3,10 @@ class Stock < ApplicationRecord
   has_many :users, through: :portfolios
   has_many :trades
 
-  def self.search_symbol(query)
+  def self search_symbol(query)
     RestClient::Request.execute(
       method: :get,
       url: "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=#{query}&apikey=#{ENV['ALPHA_VANTAGE_API_KEY']}")
   end
-  
+
 end
