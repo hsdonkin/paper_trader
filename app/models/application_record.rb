8aa9eb7b1e86f95207b89c4f1edb0f670a18f86c
Josh Mabry <mabry1985@gmail.com>
2019-08-26 22:28:29 -0700

    add search endpoint and populate stock table class method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,10 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def self.search_symbol(query)
-    RestClient::Request.execute(
-      method: :get,
-      url: "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=#{query}&apikey=#{ENV['ALPHA_VANTAGE_API_KEY']}")
-  end
+
 
 end
