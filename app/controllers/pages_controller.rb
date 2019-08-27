fc7e502776d8493b45a71ce54c580612dc554870
Josh Mabry <mabry1985@gmail.com>
2019-08-27 14:16:16 -0700

    add search bar to nav and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,17 @@
+require 'byebug'
+
 class PagesController < ApplicationController
+
   def index
     render :index
   end
 
   def search
     if params[:search].blank?
-      redirect_to(root_path, alert: "Empty field!") and return
+      redirect_to(search_path, alert: "Empty field!") and return
     else
       @stock = Stock.populate_stock_table(params[:search])
     end
-
-     render :search
+    redirect_to stock_path(@stock)
   end
 end
