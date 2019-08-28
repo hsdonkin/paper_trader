943a049b1da17cce75436d8d5e274170afd7ee82
Student <example@example.com>
2019-08-28 10:32:28 -0700

    fix routing for buy sell forms

--- app/models/value.rb
+++ app/models/value.rb
@@ -6,7 +6,7 @@ class Value < ApplicationRecord
     stock = Stock.find_by_symbol(symbol)
 
     JSON.parse(Stock::Call.daily(symbol))["Time Series (30min)"].each do |t|
-      value = Value.new(:price => t[1]["4. close"], :log_time => Time.now(), :stock_id => stock.id)
+      value = Value.new(:price => t[1]["4. close"], :log_time => t[0], :stock_id => stock.id)
 
       value.save!()
     end
