c85ad31fbfc59f70f170b38e5bb1418a5abb7847
Student <example@example.com>
2019-08-29 09:18:40 -0700

    Add leaderboard functionality.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,9 +1,12 @@
 require 'byebug'
 class PortfoliosController < ApplicationController
   def index
-
-    @stocks = Portfolio.stock_summary(current_user)
-    @trades = Portfolio.trade_summary(current_user)
-    render :index
+    if current_user
+      @stocks = Portfolio.stock_summary(current_user)
+      @trades = Portfolio.trade_summary(current_user)
+      @total_equity = Portfolio.total_equity(current_user)
+      @leaderboard = Portfolio.leaderboard
+      render :index
+    end
   end
 end
