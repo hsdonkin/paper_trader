1cf61dd23df82d3b267a07ad171257f989578b71
Josh Mabry <mabry1985@gmail.com>
2019-08-29 08:35:07 -0700

    move print for populate ruby method

--- app/models/value.rb
+++ app/models/value.rb
@@ -5,7 +5,6 @@ class Value < ApplicationRecord
     stock = Stock.find_by_symbol(symbol)
     JSON.parse(Stock::Call.daily(symbol))["Time Series (15min)"].each do |t|
       if t[0].split[0].to_time > Date.today
-        print "t.[1] entered into database"
         value = Value.new(:price => t[1]["4. close"], :log_time => t[0], :stock_id => stock.id)
         value.save!()
       end
@@ -13,6 +12,7 @@ class Value < ApplicationRecord
   end
 
   def self.populate_value_check(symbol)
+    print "t.[1] entered into database"
     stock = Stock.find_by_symbol(symbol)
     if Value.find_by_stock_id(stock.id) != nil
       if stock.values.first.created_at < Time.now.utc - 500
