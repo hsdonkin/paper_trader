558fb53122fc0d2dc68616f627cf6a1a65247722
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-09-07 14:13:05 -0700

    add flash notices for buy / sell, fixes glitch where negative sales would drop portfolio worth

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,18 +1,26 @@
 class TradesController < ApplicationController
   def create
     stock = Stock.find_by_id(params[:stock_id].to_i)
+    # conditional to prevent buying / selling that you aren't able to do
     if params[:commit] == "Buy"
-      current_user.buy_stock(current_user, stock, params[:buy_qty].to_i)
-      current_user.portfolio_update(current_user)
-      puts "i am up here"
+      if current_user.buy_stock(current_user, stock, params[:buy_qty].to_i) == false
+        flash[:notice] = {:msg => "Trade not authorized, insufficent funds.", :class=> "trade-fail-flash"}
+      else
+        flash[:notice] = {:msg => "Trade successful!", :class=> "trade-success-flash"}
+        current_user.portfolio_update(current_user)
+      end
       redirect_to stock_path(stock)
+
     elsif params[:commit] == "Sell"
-      current_user.sell_stock(current_user, stock, params[:sell_qty].to_i)
-      current_user.portfolio_update(current_user)
-      puts "i am over here"
+      if current_user.sell_stock(current_user, stock, params[:sell_qty].to_i) == false
+        flash[:notice] = {:msg => "Trade not authorized, insufficient stock.", :class=> "trade-fail-flash"}
+      else
+        flash[:notice] = {:msg => "Trade successful!", :class=> "trade-success-flash"}
+        current_user.portfolio_update(current_user)
+      end
       redirect_to stock_path(stock)
     end
-    puts "right here boss"
+
 
   end
 end
