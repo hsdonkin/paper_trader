9ac65ae4e69e10630dd1ea9bbf248f09022a38e7
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-09-07 14:24:51 -0700

    add styling to flash notices

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -4,18 +4,18 @@ class TradesController < ApplicationController
     # conditional to prevent buying / selling that you aren't able to do
     if params[:commit] == "Buy"
       if current_user.buy_stock(current_user, stock, params[:buy_qty].to_i) == false
-        flash[:notice] = {:msg => "Trade not authorized, insufficent funds.", :class=> "trade-fail-flash"}
+        flash[:notice] = {:msg => " \u25B6 Trade not authorized, insufficent funds.", :class=> "trade-fail-flash"}
       else
-        flash[:notice] = {:msg => "Trade successful!", :class=> "trade-success-flash"}
+        flash[:notice] = {:msg => "\u25B6 Trade successful!", :class=> "trade-success-flash"}
         current_user.portfolio_update(current_user)
       end
       redirect_to stock_path(stock)
 
     elsif params[:commit] == "Sell"
       if current_user.sell_stock(current_user, stock, params[:sell_qty].to_i) == false
-        flash[:notice] = {:msg => "Trade not authorized, insufficient stock.", :class=> "trade-fail-flash"}
+        flash[:notice] = {:msg => "\u25B6 Trade not authorized, insufficient stock.", :class=> "trade-fail-flash"}
       else
-        flash[:notice] = {:msg => "Trade successful!", :class=> "trade-success-flash"}
+        flash[:notice] = {:msg => "\u25B6 Trade successful!", :class=> "trade-success-flash"}
         current_user.portfolio_update(current_user)
       end
       redirect_to stock_path(stock)
